d2bfd2d7ad60e4a9ffa863d3629925967c4f9570
ridge23 <ridgem.sibuyi@gmail.com>
2026-08-06 14:12:27 +0200

    add task completion toggle

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const addTaskButton = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
 
-
-
 addTaskButton,addEventListener("click", function () {
     const taskText = taskInput.value.trim();
 
@@ -15,10 +13,13 @@ addTaskButton,addEventListener("click", function () {
     const taskItem = document.createElement("li");
     taskItem.textContent = taskText;
 
+    taskItem.addEventListener("click", function() {
+        taskItem.classList.toggle("completed")
+    });
+
     taskList.appendChild(taskItem);
 
     taskInput.value = "";
 
-})
+});
 
-console.log(taskList)
