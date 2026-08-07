5d9d47319ac147ab1892b53500f307a0083abe66
ridge23 <ridgem.sibuyi@gmail.com>
2026-08-07 12:29:39 +0200

    Save tasks with localStorage

--- script.js
+++ script.js
@@ -2,7 +2,53 @@ const taskInput = document.getElementById("task-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach(task => {
+        const taskItem = document.createElement("li");
+        taskItem.textContent = task.text; 
+
+        if (task.completed) {
+            taskItem.classList.add("completed");
+        }
+
+        taskItem.addEventListener("click", function() {
+            task.completed = !task.completed;
+
+            saveTasks();
+            renderTasks();
+        });
+
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-btn");
+
+
+        deleteButton.addEventListener("click", function () {
+            event.stopPropagation();
+            
+            tasks = tasks.filter(function (currentTask) {
+                return currentTask.id !== task.id;
+            });
+
+            saveTasks();
+            renderTasks();
+        });
+
+        taskItem.appendChild(deleteButton);
+        taskList.appendChild(taskItem);
+
+    });
+
+}
 
 function addTask() {
     const taskText = taskInput.value.trim();
@@ -11,36 +57,29 @@ function addTask() {
         return;
     }
 
-    const taskItem = document.createElement("li");
-    taskItem.textContent = taskText;
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    };
 
-    taskItem.addEventListener("click", function() {
-        taskItem.classList.toggle("completed")
-    });
+    tasks.push(newTask);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("delete-btn");
-
-    deleteButton.addEventListener("click", function () {
-        event.stopPropagation();
-        taskItem.remove();
-    });
-
-    taskItem.appendChild(deleteButton);
-    taskList.appendChild(taskItem);
-
+    saveTasks();
+    renderTasks();
 
     taskInput.value = "";
     taskInput.focus();
-
 }
+
 addTaskButton,addEventListener("click", addTask);
 
 taskInput.addEventListener("keydown", function (event){
     if (event.key === "Enter") {
         addTask();
     }
-})
+});
+
+renderTasks();
 
 
